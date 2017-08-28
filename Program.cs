6b8a9f125aa73842c818a2c85f361d428c215c02
xtuxur <alex.delzar@gmail.com>
2017-08-28 18:38:46 -0500

    COMMIT INICIAL

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace agua
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+
+
+
+        ///<summary>
+        ///Calcula el número de botella dependiendo de los minutos 
+        ///cada minuto equivale a 12 botellas
+        ///</summary>
+        static int calcularBotella(int minutos)
+        {
+            return 0;
+        }
+
+        ///<summary>
+        ///Calcula el número de días restantes hasta la fecha dada
+        ///</summary>
+        static int calcularDiasRestantes(DateTime fechaCumple)
+        {
+            return 0;
+        }
+    }
+}
